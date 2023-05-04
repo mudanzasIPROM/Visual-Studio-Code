2fd76455ccefc5316a31419208286f50f20b3ccf
mudanzasIPROM <118130796+mudanzasIPROM@users.noreply.github.com>
2023-05-04 21:28:38 +0200

    abbc

---
+++ fundi.ps1
@@ -0,0 +1,31 @@
+﻿#Fundi1
+Import-Module Selenium
+Import-Module Microsoft.WSMan.Management
+Import-Module Microsoft.PowerShell.Management
+Import-Module ISE
+Import-Module Microsoft.PowerShell.Utility
+Import-Module LanguageDsc
+Import-Module PackageManagement
+Import-Module PowerShellGet
+Import-Module PSReadLine
+Import-Module NuGet
+
+$Driver = Start-SeChrome
+$Driver = ChromeDriver
+
+#To open a new tab
+Enter-SeUrl https://www.google.com/ -Driver $Driver
+$Driver.FindElementByClassName("GzLjMd").click()
+$Driver.FindElementByName("q").SendKeys("transporte de pianos barcelona")
+$Driver.FindElementByName("q").Submit()
+Sleep -Seconds(3.8)
+$Driver.FindElementByClassName("d8lRkd").Click()
+Sleep -Seconds(9.41)
+SeChrome = switchTo.window(tabs.get(1))
+
+#To navigate to new link/URL in 2nd new tab
+Enter-SeUrl https://www.google.com/ -Driver $Driver
+
+
+
+$Driver.SwitchTo().Window($driver.WindowHandles2)
---
+++ rosa.java
@@ -0,0 +1,38 @@
+# py
+from typing import Self
+input()
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+class TestChromeSelenium(unittest.TestCase):
+
+    Self.driver = webdriver.Chrome(executable_path=r"C:\Program Files\PowerShell\7-preview\Modules\Selenium\3.0.1\assemblies\chromedriver.exe")
+    def test_pagin_siguient_o_anterior(self):
+     driver = self.driver      
+     driver.get("https://www.google.es/")
+     driver.set_window_size(550, 691)
+     driver.find_element(By.ID, "APjFqb").click()
+     driver.find_element(By.ID, "APjFqb").send_keys("traslado de pianos")
+     driver.find_element(By.CSS_SELECTOR, ".QCzoEc > svg").click()
+     driver.find_element(By.NAME, "btnK").click()
+     driver.find_element(By.CSS_SELECTOR, ".uEierd:nth-child(3) .v5yQqb").click()
+     driver.execute_script("window.scrollTo(0,59)")
+     driver.find_element(By.CSS_SELECTOR, "#tads > .uEierd:nth-child(3) .CCgQ5 > span").click()
+     driver.execute_script("window.scrollTo(0,59)")
+     driver.execute_script("window.ope('');")
+     time.sleep(3)
+     driver.switchTo().window()
+     driver = self.driver      
+     driver.get("https://www.google.es/")
+     driver.set_window_size(550, 691)
+     driver.find_element(By.ID, "APjFqb").click()
+     driver.find_element(By.ID, "APjFqb").send_keys("traslado de pianos")
+     driver.find_element(By.CSS_SELECTOR, ".QCzoEc > svg").click()
+     driver.find_element(By.NAME, "btnK").click()
+     driver.find_element(By.CSS_SELECTOR, ".uEierd:nth-child(3) .v5yQqb").click()
+     driver.execute_script("window.scrollTo(0,59)")
+     driver.find_element(By.CSS_SELECTOR, "#tads > .uEierd:nth-child(3) .CCgQ5 > span").click()
+     driver.execute_script("window.scrollTo(0,59)")
+     driver.execute_script("window.ope('');")
+     driver.quit()
