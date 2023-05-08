3b985dca5182a0698fae82c46a27336e22df4049
mudanzasIPROM <118130796+mudanzasIPROM@users.noreply.github.com>
2023-05-08 19:12:53 +0200

    28/05/2023

--- fundi.ps1
+++ fundi.ps1
@@ -1,18 +1,11 @@
 ﻿#Fundi1
+
 Import-Module Selenium
 Import-Module Microsoft.WSMan.Management
-Import-Module Microsoft.PowerShell.Management
-Import-Module ISE -force
 Import-Module Microsoft.PowerShell.Utility
-Import-Module LanguageDsc
-Import-Module PowerShellGet -force
-Import-Module PSReadLine -force
-Import-Module NuGet -force
 
-$Driver = Start-SeChrome
-$Driver.OpenQA.Selenium.IWebDriver
-$NewTab = Crtl + "t"
-$Driver = Start Chrome https://www.google.com
+#click(Ctrl+t)
+$Driver = Start-Sechrome https://www.google.com 
 document.querySelector("#APjFqb")
 $Driver.FindElementByClassName("GzLjMd").click()
 $Driver.FindElementByName("q").SendKeys("transporte de pianos barcelona")
@@ -20,5 +13,5 @@ $Driver.FindElementByName("q").Submit()
 Sleep -Seconds(1.5)
 $Driver.FindElementByClassName("d8lRkd").Click()
 Sleep -Seconds(9.41)
-Start chrome https://www.google.com/ (Taget="_blank") 
-$Driver.SwitchTo().Window.Newtabs23
+$Driver = Open-SeUrl https://www.google.com/?new-tab
+
