6734d94a6f9328d52faf843a3574eb458b3fcff0
mudanzasIPROM <118130796+mudanzasIPROM@users.noreply.github.com>
2023-05-19 01:13:56 +0200

    19/05

--- fundi.ps1
+++ fundi.ps1
@@ -3,9 +3,8 @@
 Import-Module Selenium 
 Import-Module Microsoft.WSMan.Management
 Import-Module Microsoft.PowerShell.Utility
-$Driver=Start "C:\Users\user\Desktop\Visual Studio Code\chromedriver\chromeDriver.exe"
-$Driver=New-Object OpenQA.Selenium.Chrome.ChromeDriver
-$Driver.Navigate().GoToUrl("https://www.google.com/search?q=transporte+de+pianos+barcelona")
+$Driver = New-SeDriverOptions OpenQA.Selenium.Chrome.ChromeDriver
+$Driver = Start-SeDriver ("https://www.google.com/search?q=transporte+de+pianos+barcelona")
 Sleep -Seconds(1.5)
 $Driver.FindElementByClassName("wbJOMb").Click()
 Sleep -Seconds(9.41)
